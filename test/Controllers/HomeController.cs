47c5ddd0482b3d98f3e11da175f0fe131322493c
felixyu <felixyu@accuvally.com>
2017-03-10 14:59:43 +0800

    no message

---
+++ test/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using test.Models;
+namespace test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            TestClass t = new TestClass();
+
+            string a = t.str;
+            t.str = "456";
+
+            a = t.str;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ test/Models/TestModels.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace test.Models
+{
+    public class TestClass
+    {
+        private string _str = "123";  //初始宣告
+        public string str
+        {
+            get { return _str; }
+            set { _str = value; }
+
+        }
+
+    }
+}
